3a26e15c4af127e490f91efa3e846358dc287b6d
AntonioLuis3181 <xavevel449@ieshnosmachado.org>
2025-10-29 12:27:23 +0100

    base de datos

--- assets/sql/backup.sql
+++ assets/sql/backup.sql
@@ -1,34 +1,33 @@
 -- Base de datos: tienda_ropa
-CREATE DATABASE IF NOT EXISTS nova_vibe CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+CREATE DATABASE IF NOT EXISTS tienda_ropa CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 
-USE nova_vibe;
+USE tienda_ropa;
 
--- Tabla: usuarios
-CREATE TABLE usuarios (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(100) NOT NULL,
-    email VARCHAR(150) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL
+CREATE TABLE categorias (
+    id_categoria INT AUTO_INCREMENT PRIMARY KEY,
+    nombre_categoria VARCHAR(100) NOT NULL,
+    descripcion VARCHAR(255),
+    fecha_creacion DATETIME DEFAULT CURRENT_TIMESTAMP,
+    partes INT,
+    parte_de_arriba BOOLEAN DEFAULT 0
 );
 
--- Tabla: productos
 CREATE TABLE productos (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(150) NOT NULL,
-    descripcion TEXT,
+    id_producto INT AUTO_INCREMENT PRIMARY KEY,
+    nombre_producto VARCHAR(100) NOT NULL,
     precio DECIMAL(10, 2) NOT NULL,
-    stock INT DEFAULT 0,
-    imagen VARCHAR(255)
+    id_categoria INT,
+    en_stock BOOLEAN DEFAULT 1,
+    fecha_registro DATE DEFAULT(CURRENT_DATE),
+    FOREIGN KEY (id_categoria) REFERENCES categorias (id_categoria) ON DELETE SET NULL ON UPDATE CASCADE
 );
 
--- Tabla: pedidos
-CREATE TABLE pedidos (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    id_usuario INT NOT NULL,
-    id_producto INT NOT NULL,
-    cantidad INT DEFAULT 1,
-    total DECIMAL(10, 2) NOT NULL,
-    fecha DATETIME DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (id_usuario) REFERENCES usuarios (id) ON DELETE CASCADE,
-    FOREIGN KEY (id_producto) REFERENCES productos (id) ON DELETE CASCADE
+CREATE TABLE ventas (
+    id_venta INT AUTO_INCREMENT PRIMARY KEY,
+    fecha_venta DATETIME DEFAULT CURRENT_TIMESTAMP,
+    cantidad_producto INT NOT NULL,
+    id_producto INT,
+    venta_online BOOLEAN DEFAULT 0,
+    descripcion VARCHAR(1000),
+    FOREIGN KEY (id_producto) REFERENCES productos (id_producto) ON DELETE SET NULL ON UPDATE CASCADE
 );
