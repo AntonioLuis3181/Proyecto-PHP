0de083774a887a0c10a80a03cb1fdce86391a74e
AntonioLuis3181 <xavevel449@ieshnosmachado.org>
2025-11-11 09:21:57 +0100

    11/11/2025

---
+++ admin.php
@@ -0,0 +1,28 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="css/bootstrap.min.css" rel="stylesheet" />
+    <title>Tienda: Administrador</title>
+</head>
+
+<body>
+    <div class="container">
+        <h1>Acciones de administrador</h1>
+        <ul class="list-group">
+            <li class="list-group-item"> <a href="altaCategoria.php">Alta de categoría</a> </li>
+            <li class="list-group-item"> <a href="altaProducto.php">Alta de producto</a> </li>
+        </ul>
+    </div>
+
+    <script src="js/bootstrap.min.js"></script>
+</body>
+
+</html>
--- funcionesUtiles.php
+++ funcionesUtiles.php
@@ -1,21 +1,40 @@
 <?php
+function obtenerConexion()
+{
+    $servidor = "db";
+    $usuario = "root";
+    $contrasena = ""; // o la contraseña que uses
+    $baseDatos = "nova_vibe";
+
+    $conexion = new mysqli($servidor, $usuario, $contrasena, $baseDatos);
+
+    if ($conexion->connect_error) {
+        die("Error de conexión: " . $conexion->connect_error);
+    }
+
+    return $conexion;
+}
+
 function comprobarSesionAdmin()
 {
     if (!isset($_SESSION["conectado"]) || $_SESSION["conectado"] != "admin") {
-        header("Location: index.php");
+        header("Location: admin.php");
+        exit();
     }
 }
 
 function comprobarSesionCliente()
 {
     if (!isset($_SESSION["conectado"]) || $_SESSION["conectado"] != "cliente") {
-        header("Location: index.php");
+        header("Location: admin.php");
+        exit();
     }
 }
 
 function comprobarSesionClienteOAdmin()
 {
     if (!isset($_SESSION["conectado"])) {
-        header("Location: index.php");
+        header("Location: admin.php");
+        exit();
     }
 }
